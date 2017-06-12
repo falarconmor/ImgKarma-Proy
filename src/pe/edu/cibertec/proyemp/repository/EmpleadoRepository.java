65390dd31231b3480a53eb6cb39a88132706e7f9
falarconmor <flavio_alarcon3@hotmail.com>
2017-06-12 14:37:45 -0500

    s
    s

---
+++ src/pe/edu/cibertec/proyemp/repository/EmpleadoRepository.java
@@ -0,0 +1,21 @@
+package pe.edu.cibertec.proyemp.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import pe.edu.cibertec.proyemp.model.Empleado;
+
+@Repository
+public interface EmpleadoRepository
+	extends JpaRepository<Empleado, Long>{
+
+	
+	@Query("select e from Empleado e where e.nombre like %:nom%")
+	List<Empleado> obtenerEmpleadosPorNombre(
+			@Param("nom") String nombre);
+	
+}
